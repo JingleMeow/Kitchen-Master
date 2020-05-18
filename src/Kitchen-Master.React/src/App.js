9cd65981e8401dbb3e76ce4e27d2c5e836e84dda
JingleMeow <tongmh2006@gmail.com>
2020-05-17 22:22:21 -0400

    Added eslint and fixed format problems.

--- src/Kitchen-Master.React/src/App.js
+++ src/Kitchen-Master.React/src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
-import { hot } from 'react-hot-loader';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Test from "./component/test";
-//import "./App.css";
+import React, {Component} from 'react';
+import {hot} from 'react-hot-loader';
+import {BrowserRouter, Switch} from 'react-router-dom';
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <BrowserRouter>
-                    <h1> Hello, World!</h1>
-                    <Switch>
-                    </Switch>
-                </BrowserRouter>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <h1> Hello, World!</h1>
+          <Switch>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default hot(module)(App);
--- src/Kitchen-Master.React/webpack.config.js
+++ src/Kitchen-Master.React/webpack.config.js
@@ -1,54 +1,54 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: "babel-loader",
-                options: { presets: ["@babel/env"] }                
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', "css-loader", 'sass-loader']
-            },
-            {
-                test: /\.(eot|ttf|woff2?|otf|svg|png|jpe?g|gif)$/,
-                loader: 'file-loader',
-                options: {
-                    name(resourcePath, resourceQuery) {
-                        // `resourcePath` - `/absolute/path/to/file.js`
-                        // `resourceQuery` - `?foo=bar`
-                        if (process.env.NODE_ENV === 'development') {
-                            return '[path][name].[ext]';
-                        }
-                        return '[contenthash].[ext]';
-                    },
-                    outputPath: 'images'
-                }
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {presets: ['@babel/env']},
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(eot|ttf|woff2?|otf|svg|png|jpe?g|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name(resourcePath, resourceQuery) {
+            // `resourcePath` - `/absolute/path/to/file.js`
+            // `resourceQuery` - `?foo=bar`
+            if (process.env.NODE_ENV === 'development') {
+              return '[path][name].[ext]';
             }
-        ]
-    },
-    resolve: { extensions: [".js", ".jsx"] },
-    output: {
-        path: path.resolve(__dirname, "dist/"),
-        publicPath: "/dist/",
-        filename: "bundle.js"
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "public/"),
-        port: 3000,
-        publicPath: "http://localhost:3000/dist/",
-        hotOnly: true,
-        historyApiFallback: true
-    },
-    plugins: [new webpack.HotModuleReplacementPlugin()]
+            return '[contenthash].[ext]';
+          },
+          outputPath: 'images',
+        },
+      },
+    ],
+  },
+  resolve: {extensions: ['.js', '.jsx']},
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true,
+    historyApiFallback: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 };
