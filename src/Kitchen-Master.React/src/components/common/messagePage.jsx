13abf6aafad8061da2a47c8949568b3ad69c89ea
JingleMeow <tongmh2006@gmail.com>
2020-07-26 18:24:59 -0400

    Adjust message page to fit mobile better.

--- src/Kitchen-Master.React/src/components/common/messagePage.jsx
+++ src/Kitchen-Master.React/src/components/common/messagePage.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Message, Container, Icon } from 'semantic-ui-react';
+import { Message, Container, Icon, Responsive, Divider } from 'semantic-ui-react';
 
 const MessagePage = ({ title, messageType, children }) => {
     return (
         <Container>
             <br />
-            <Message icon {...messageType.type}>
-                <Icon name={messageType.icon} />
-                <Message.Content>
-                    <Message.Header>{title}</Message.Header>
-                    {children}
-                </Message.Content>
-            </Message>
+            <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+                <Message size='massive' icon {...messageType.type}>
+                    <Icon name={messageType.icon} size='huge' />
+                    <Message.Content>
+                        <Message.Header>{title}</Message.Header>
+                        {children}
+                    </Message.Content>
+                </Message>
+            </Responsive>
+            <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
+                <Message size='massive' {...messageType.type}>
+                    <div style={{ textAlign: "center" }}>
+                        <Icon name={messageType.icon} size='huge' />
+                    </div>
+                    <Divider section />
+                    <Message.Content>
+                        <Message.Header>{title}</Message.Header>
+                        {children}
+                    </Message.Content>
+                </Message>
+            </Responsive>
         </Container>
     );
 };
+
 MessagePage.propTypes = {
     title: PropTypes.string.isRequired,
     messageType: PropTypes.object.isRequired
