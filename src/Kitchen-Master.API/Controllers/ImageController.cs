a4a5ac98470bcf5942ce207befad2307531b1e24
JingleMeow <tongmh2006@gmail.com>
2020-08-04 21:48:44 -0400

    Add backend service for image upload

---
+++ src/Kitchen-Master.API/Controllers/ImageController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Kitchen_Master.API.Services.Image;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Kitchen_Master.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ImageController : ControllerBase
+    {
+        private readonly ImageService _imageService;
+
+        public ImageController(ImageService imageService)
+        {
+            this._imageService = imageService;
+        }
+
+        [HttpPut("upload")]
+        public async Task<ActionResult<string>> Upload([FromForm(Name = "image")] List<IFormFile> images)
+        {
+            var fileName = await _imageService.SaveImage(images[0]);
+            return Ok(fileName);
+        }
+    }
+}
---
+++ src/Kitchen-Master.API/Services/Image/ImageService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitchen_Master.API.Services.Image
+{
+    public class ImageService : IFeatureService
+    {
+        private readonly IConfiguration _configuration;
+
+        public ImageService(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public async Task<string> SaveImage(IFormFile imageFormFile)
+        {
+            var stringBuilder = new StringBuilder();
+            using (var sha256 = SHA256.Create())
+            {
+                var hashBytes = sha256.ComputeHash(imageFormFile.OpenReadStream());
+                foreach (var b in hashBytes)
+                {
+                    stringBuilder.Append(b.ToString("x2"));
+                }
+            }
+            var fileName = stringBuilder.ToString();
+
+            var filePath = Path.Combine(this._configuration.GetSection("ImagesFolder").Value, $"{fileName}.jpg");
+            if (!File.Exists(filePath))
+            {
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await imageFormFile.CopyToAsync(fileStream);
+                }
+            }
+            return fileName;
+        }
+    }
+}
