229d0e114e5494f01d9402b164d59dc539c0c2f9
JingleMeow <tongmh2006@gmail.com>
2020-09-08 08:45:00 -0400

    Add tooltip to like button

--- src/Kitchen-Master.React/src/components/recipe/view/like/likeButton.jsx
+++ src/Kitchen-Master.React/src/components/recipe/view/like/likeButton.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import withLikeWrapper from '../../common/like/withLikeWrapper';
-import { Button, Icon, Label } from 'semantic-ui-react';
+import { Button, Icon, Label, Popup } from 'semantic-ui-react';
 
 const LikeButton = ({ likes, likeClickable, isLiked, isLoadingLiked, handleLike }) => {
     const color = isLiked ? 'red' : 'grey';
+    const tooltip = isLiked ? 'Unlike' : 'I like this';
     return (
-        <Button as='div' fluid labelPosition='right' disabled={!likeClickable || isLoadingLiked} onClick={handleLike} >
-            <Button fluid color={color}>
-                <Icon name='heart' />
-                Like
-            </Button>
-            <Label as='a' basic color={color} pointing='left'>
-                {likes}
-            </Label>
-        </Button>
+        <Popup
+            trigger={
+                <Button as='div' fluid labelPosition='right' disabled={!likeClickable || isLoadingLiked} onClick={handleLike} >
+                    <Button fluid color={color}>
+                        <Icon name='heart' />
+                    Like
+                </Button>
+                    <Label as='a' basic color={color} pointing='left'>
+                        {likes}
+                    </Label>
+                </Button>
+            }
+            position='bottom center'>
+            {tooltip}
+        </Popup>
     );
 }
 
