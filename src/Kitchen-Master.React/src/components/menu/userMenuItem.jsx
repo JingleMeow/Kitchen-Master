a3da8d26954d3626a20e36de10cbb03d9ae7d748
JingleMeow <tongmh2006@gmail.com>
2020-09-09 21:53:44 -0400

    Menu page for mobile

--- src/Kitchen-Master.React/src/components/menu/userMenuItem.jsx
+++ src/Kitchen-Master.React/src/components/menu/userMenuItem.jsx
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import { Grid, Image, Button, Header } from 'semantic-ui-react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Image, Button, Responsive } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 import { getImageUrl } from '../../utils/recipeUtils';
 import styles from './userMenuItem.module.scss';
-import { Link } from 'react-router-dom';
 
 const UserMenuItem = ({ recipe, onRemove }) => {
     return (
         <div className={styles.row}>
             <Image bordered src={getImageUrl(recipe.coverImageId)} className={styles.recipeImage}></Image>
-            <div className={styles.title}>
-                <Link to={`/recipe/${recipe.id}`}>{recipe.name}</Link>
+            <div className={styles.textAndButton}>
+                <div className={styles.title}>
+                    <Link to={`/recipe/${recipe.id}`} >{recipe.name}</Link>
+                </div>
+                <Responsive minWidth={Responsive.onlyTablet.minWidth}
+                    as={Button} color='red' size='huge'
+                    icon='trash alternate'
+                    className={styles.removeButton}
+                    onClick={() => onRemove(recipe.id)} />
+                <Responsive maxWidth={Responsive.onlyMobile.maxWidth}
+                    as={Button} color='red' size='small' fluid
+                    icon='trash alternate'
+                    content='Remove Recipe'
+                    className={styles.removeButton}
+                    onClick={() => onRemove(recipe.id)} />
             </div>
-            <Button color='red' className={styles.removeButton} size='huge'
-                icon='trash alternate'
-                onClick={() => onRemove(recipe.id)} />
-        </div>
+        </div >
     );
+}
+
+UserMenuItem.propTypes = {
+    recipe: PropTypes.object.isRequired,
+    onRemove: PropTypes.func.isRequired
 }
 
 export default UserMenuItem;
